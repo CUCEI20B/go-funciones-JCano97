4749454414c9e481e41b938ce9895b8e32c58003
Eduardo Cano <jose.cano6053@alumnos.udg.mx>
2020-10-20 13:20:05 -0500

    burbuja listo

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,18 @@
 package main
 
-func Burbuja(s []int64)  {
-
+func Burbuja(s []int64) {
+	var temp int64
+	for i := 1; i < len(s); i++ {
+		for j := 0; j < (len(s) - i); j++ {
+			if s[j] > s[j+1] {
+				temp = s[j]
+				s[j] = s[j+1]
+				s[j+1] = temp
+			}
+		}
+	}
 }
 
-func main()  {
-	
+func main() {
+
 }
